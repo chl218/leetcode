98c720c34dbf947dc2e1716027b65bc41845ea61
Li, Jack <chl218@eng.ucsd.edu>
2019-05-14 20:52:54 -0700

    Best Time to Buy and Sell Stock II

--- python/01-easy-collection/leet_array.py
+++ python/01-easy-collection/leet_array.py
@@ -26,3 +26,32 @@ def remove_duplicates(nums: List[int]) -> int:
             curr += 1
 
     return curr
+
+
+def max_profit(prices: List[int]) -> int:
+    """Best Time to Buy and Sell Stock II
+
+    Say you have an array for which the ith element is the price of a given
+    stock on day i.
+
+    Design an algorithm to find the maximum profit. You may complete as many
+    transactions as you like (i.e., buy one and sell one share of the stock
+    multiple times).
+
+    Note: You may not engage in multiple transactions at the same time (i.e.,
+    you must sell the stock before you buy again).
+
+    :param
+        List of prices
+    :return:
+        Maximum profit
+    """
+
+    # Peak-Vally Approach
+    #   TotalProfit = sum_i(height(peak_i) - height(vally_i))
+    profit = 0
+    for i in range(len(prices)-1):
+        if prices[i+1] > prices[i]:
+            profit += prices[i+1] - prices[i]
+
+    return profit
