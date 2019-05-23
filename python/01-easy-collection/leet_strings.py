70cf5bec7319108dde14a8f839148778a13a971e
Li, Jack <chl218@eng.ucsd.edu>
2019-05-22 20:31:36 -0700

    atoi

--- python/01-easy-collection/leet_strings.py
+++ python/01-easy-collection/leet_strings.py
@@ -149,3 +149,70 @@ def is_palindrome(s: str) -> bool:
             j -= 1
 
     return True
+
+
+def my_atoi(s: str) -> int:
+    """String to Integer (atoi)
+
+    Implement atoi which converts a string to an integer.
+
+    The function first discards as many whitespace characters as necessary until
+    the first non-whitespace character is found. Then, starting from this
+    character, takes an optional initial plus or minus sign followed by as many
+    numerical digits as possible, and interprets them as a numerical value.
+
+    The string can contain additional characters after those that form the
+    integral number, which are ignored and have no effect on the behavior of
+    this function.
+
+    If the first sequence of non-whitespace characters in str is not a valid
+    integral number, or if no such sequence exists because either str is empty
+    or it contains only whitespace characters, no conversion is performed.
+
+    If no valid conversion could be performed, a zero value is returned.
+
+    Note:
+        Only the space character ' ' is considered as whitespace character.
+
+        Assume we are dealing with an environment which could only store
+        integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. If
+        the numerical value is out of the range of representable values,
+        INT_MAX (2^31 − 1) or INT_MIN (−2^31) is returned
+
+    """
+
+    idx = 0
+    while idx < len(s) and s[idx] == ' ':
+        idx += 1
+
+    if idx >= len(s):
+        return 0
+
+    sign = 1
+    if s[idx] == '-':
+        sign = -1
+        idx += 1
+    elif s[idx] == '+':
+        idx += 1
+    elif s[idx].isalpha():
+        return 0
+
+    n = 0
+    for i in range(idx, len(s)):
+
+        if not s[i].isdigit():
+            return n
+
+        n *= 10
+
+        if sign < 0:
+            n -= ord(s[i]) - ord('0')
+            if n < -2147483648:
+                return -2147483648
+        else:
+            n += ord(s[i]) - ord('0')
+            if n > 2147483647:
+                return 2147483647
+
+    return n
+
