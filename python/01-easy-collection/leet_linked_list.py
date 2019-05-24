9bd6b7df6ec60b057e9e986d10ce1a82f581796a
Li, Jack <chl218@eng.ucsd.edu>
2019-05-23 23:15:16 -0700

    delete node

---
+++ python/01-easy-collection/leet_linked_list.py
@@ -0,0 +1,31 @@
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+def delete_node(self, node):
+    """ Delete Node in a Linked List
+    Write a function to delete a node (except the tail) in a singly linked list,
+    given only access to that node.
+
+    Note:
+        - The linked list will have at least two elements.
+        - All of the nodes' values will be unique.
+        - The given node will not be the tail and it will always be a valid node
+            of the linked list.
+        - Do not return anything from your function.
+
+    :type node: ListNode
+    :rtype: void Do not return anything, modify node in-place instead.
+    """
+
+    deleteNode = node.next
+
+    node.next = deleteNode.next
+    node.val = deleteNode.val
+
+    deleteNode.Next = None
+
