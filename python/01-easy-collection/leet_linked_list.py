5299c66a681ec2b938c7ae2cbc0e33a7416c4ec3
Li, Jack <chl218@eng.ucsd.edu>
2019-05-31 20:20:50 -0700

    Remove Nth Node From End of List

--- python/01-easy-collection/leet_linked_list.py
+++ python/01-easy-collection/leet_linked_list.py
@@ -1,12 +1,14 @@
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
 
 
-def delete_node(node):
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def deleteNode(node: ListNode):
     """ Delete Node in a Linked List
     Write a function to delete a node (except the tail) in a singly linked list,
     given only access to that node.
@@ -17,15 +19,48 @@ def delete_node(node):
         - The given node will not be the tail and it will always be a valid node
             of the linked list.
         - Do not return anything from your function.
-
-    :type node: ListNode
-    :rtype: void Do not return anything, modify node in-place instead.
     """
 
     old_node = node.next
 
-    node.next = to_be_deleted.next
-    node.val = to_be_deleted.val
+    node.next = old_node.next
+    node.val = old_node.val
 
-    to_be_deleted.Next = None
+    old_node.Next = None
+
+
+def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
+    """Remove Nth Node From End of List
+
+    Given a linked list, remove the n-th node from the end of list and return
+    its head.
+
+    Example:
+    Given linked list: 1->2->3->4->5, and n = 2.
+    After removing the second node from the end, the linked list becomes
+    1->2->3->5.
+
+    Note: Given n will always be valid.
+    Follow up: Could you do this in one pass?
+    """
+
+    t = None
+    p = head
+    q = head
+    for i in range(n-1):
+        q = q.next
+
+    while q.next is not None:
+        t = p
+        p = p.next
+        q = q.next
+
+    if t is None:
+        head = head.next
+    else:
+        t.next = p.next
+
+    return head
+
+
 
