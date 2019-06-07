990adbadf89aa9ee5be9502e1ffcf96c00ac5fd6
Li, Jack <chl218@eng.ucsd.edu>
2019-06-06 22:27:17 -0700

    valid bst

--- python/01-easy-collection/leet_trees.py
+++ python/01-easy-collection/leet_trees.py
@@ -10,3 +10,20 @@ def maxDepth(self, root):
         return 0
 
     return max(self.maxDepth(root.left) + 1, self.maxDepth(root.right) + 1)
+
+
+def isValidBST(self, root: TreeNode) -> bool:
+    def isValid(root, minNode, maxNode):
+        if not root:
+            return True
+
+        if minNode and minNode.val >= root.val:
+            return False
+
+        if maxNode and maxNode.val <= root.val:
+            return False
+
+        return isValid(root.left, minNode, root) and isValid(root.right, root, maxNode)
+
+    return isValid(root, None, None)
+
