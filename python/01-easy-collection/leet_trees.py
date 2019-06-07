8d870e2174f4b26e3c20d15e625789ee0249eae2
Li, Jack <chl218@eng.ucsd.edu>
2019-06-06 22:31:33 -0700

    comments

--- python/01-easy-collection/leet_trees.py
+++ python/01-easy-collection/leet_trees.py
@@ -6,13 +6,35 @@ class TreeNode(object):
 
 
 def maxDepth(self, root):
+    """Maximum Depth of Binary Tree
+
+    Given a binary tree, find its maximum depth.
+
+    The maximum depth is the number of nodes along the longest path from the
+    root node down to the farthest leaf node.
+
+    Note: A leaf is a node with no children.
+    """
+
     if not root:
         return 0
 
     return max(self.maxDepth(root.left) + 1, self.maxDepth(root.right) + 1)
 
 
-def isValidBST(self, root: TreeNode) -> bool:
+def isValidBST(root: TreeNode) -> bool:
+    """Validate Binary Search Tree
+
+    Given a binary tree, determine if it is a valid binary search tree (BST).
+
+    Assume a BST is defined as follows:
+        - The left subtree of a node contains only nodes with keys less than the
+          node's key.
+        - The right subtree of a node contains only nodes with keys greater than
+          the node's key.
+        - Both the left and right subtrees must also be binary search trees.
+    """
+
     def isValid(root, minNode, maxNode):
         if not root:
             return True
@@ -23,7 +45,7 @@ def isValidBST(self, root: TreeNode) -> bool:
         if maxNode and maxNode.val <= root.val:
             return False
 
-        return isValid(root.left, minNode, root) and isValid(root.right, root, maxNode)
+        return isValid(root.left, minNode, root) and \
+               isValid(root.right, root, maxNode)
 
     return isValid(root, None, None)
-
