a5c5e220a6dae6bd53fd627b0994ff23c48b3500
Li, Jack <chl218@eng.ucsd.edu>
2019-06-12 21:52:25 -0700

    stair climb

---
+++ python/01-easy-collection/leet_dp.py
@@ -0,0 +1,28 @@
+
+
+def climbStairs(n: int) -> int:
+    """Climbing Stairs
+
+    You are climbing a stair case. It takes n steps to reach to the top.
+
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can
+    you climb to the top?
+
+    Note: Given n will be a positive integer.
+    """
+    dict = {1: 1}
+
+    def count(i):
+        if i <= 1:
+            return 1
+
+        c1 = dict[i - 1] if i - 1 in dict else count(i - 1)
+        c2 = dict[i - 2] if i - 2 in dict else count(i - 2)
+
+        dict[i] = c1 + c2
+        return c1 + c2
+
+    for i in range(1, n + 1):
+        count(i)
+
+    return dict[n]
