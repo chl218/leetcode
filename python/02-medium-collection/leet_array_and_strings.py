8bafddc99271f513a429e6d7a5af3f07ca239011
Li, Jack <chl218@eng.ucsd.edu>
2019-10-17 21:30:55 -0700

    3 sums

--- python/02-medium-collection/leet_array_and_strings.py
+++ python/02-medium-collection/leet_array_and_strings.py
@@ -1,5 +1,57 @@
 from typing import List
 
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    """3 Sums
+
+    Given an array nums of n integers, are there elements a, b, c in nums such
+    that a + b + c = 0? Find all unique triplets in the array which gives the
+    sum of zero.
+
+    Note:
+        - The solution set must not contain duplicate triplets.
+
+    Example:
+        Given array nums = [-1, 0, 1, 2, -1, -4]
+        A solution set is: [[-1, 0, 1], [-1, -1, 2]]
+    """
+    
+    res = []
+    nums.sort()
+    
+    if nums[0] > 0 or nums[-1] < 0:
+        return res
+    
+    for i in range(0, len(nums)-2):
+        
+        # Sum will always be creater than 0
+        if nums[i] > 0:
+            return res
+        
+        # Duplicate guard
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+            
+        j = i + 1
+        k = len(nums) - 1
+
+        while j < k:
+            _3sum = nums[i] + nums[j] + nums[k] 
+            if _3sum == 0:
+                res.append([nums[i], nums[j], nums[k]])
+
+                # Get all other matching conditions
+                while j < k and nums[j] == nums[j+1]: j += 1
+                while k > j and nums[k] == nums[k-1]: k -= 1
+                j += 1
+                k -= 1
+            elif _3sum > 0:
+                k -= 1
+            else:
+                j += 1
+
+    return res    
+
+
 def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
     """Group Anagrams
 
