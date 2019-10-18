e397df009defc892821b4be4c1ff28ac740d92d8
chl218 <chl218@eng.ucsd.edu>
2019-10-17 20:08:23 -0700

    group anagram

---
+++ python/02-medium-collection/leet_array_and_strings.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+
+
+
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    """Group Anagrams
+
+    Given an array of strings, group anagrams together.
+
+    Example:
+        Input:  ["eat", "tea", "tan", "ate", "nat", "bat"],
+        Output: [["ate","eat","tea"], ["nat","tan"], ["bat"]]
+
+    Note:
+        - All inputs will be in lowercase.
+        - The order of your output does not matter.
+    """
+    lst = []
+    dic = {}
+    idx = 0
+    
+    for word in strs:
+        key = ''.join(sorted(word))
+        
+        if key in dic:
+            lst[dic[key]].append(word)
+        else:
+            lst.append([word])
+            dic[key] = idx
+            idx += 1
+            
+    return lst
