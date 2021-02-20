3488b2cd2962ddd9aa7c7180dfcceef99c507b58
Li, Jack <chl218@eng.ucsd.edu>
2021-02-19 23:29:33 -0800

    minimum remove to make valid parenthesis

--- python/leetcoding-challenge/02-feb/peeking_iterator.py
+++ python/leetcoding-challenge/02-feb/peeking_iterator.py
@@ -1,16 +1,18 @@
-# Given an Iterator class interface with methods: next() and hasNext(), design and implement a PeekingIterator that
-# support the peek() operation -- it essentially peek() at the element that will be returned by the next call to
-# next().
-#
-# Example:
-#
-# Assume that the iterator is initialized to the beginning of the list: [1,2,3].
-#
-# Call next() gets you 1, the first element in the list.
-# Now you call peek() and it returns 2, the next element. Calling next() after that still return 2.
-# You call next() the final time and it returns 3, the last element.
-# Calling hasNext() after that should return false.
-#
+"""
+Given an Iterator class interface with methods: next() and hasNext(), design and implement a PeekingIterator that
+support the peek() operation -- it essentially peek() at the element that will be returned by the next call to
+next().
+
+Example:
+
+Assume that the iterator is initialized to the beginning of the list: [1,2,3].
+
+Call next() gets you 1, the first element in the list.
+Now you call peek() and it returns 2, the next element. Calling next() after that still return 2.
+You call next() the final time and it returns 3, the last element.
+Calling hasNext() after that should return false.
+
+"""
 
 # Below is the interface for Iterator, which is already defined for you.
 #
@@ -32,6 +34,7 @@
 #         Returns the next element in the iteration.
 #         :rtype: int
 #         """
+
 
 class PeekingIterator:
     def __init__(self, iterator):
