3c40b2f10b63dc97eda2765025696b545dab9f9f
Li, Jack <chl218@eng.ucsd.edu>
2021-04-26 23:15:41 -0700

    combination sum

--- python/leet_misc.py
+++ python/leet_misc.py
@@ -13,3 +13,17 @@ def hammingWeight(n):
     return count
 
 
+
+def combinationSum(candidates, target):
+   ret = []
+   dfs(candidates, target, [], ret)
+   return ret
+
+def dfs(self, nums, target, path, ret):
+   if target < 0:
+      return
+   if target == 0:
+      ret.append(path)
+      return
+   for i in range(len(nums)):
+      self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
