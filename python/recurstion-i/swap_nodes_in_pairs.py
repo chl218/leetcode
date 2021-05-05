58b03e9ef4c3aa05d5d9752c08f1e1a620b02e17
Li, Jack <chl218@eng.ucsd.edu>
2021-05-04 21:25:19 -0700

    reverse linked list

--- python/recurstion-i/swap_nodes_in_pairs.py
+++ python/recurstion-i/swap_nodes_in_pairs.py
@@ -1,10 +1,7 @@
 """
 Given a linked list, swap every two adjacent nodes and return its head.
 
- 
-
 Example 1:
-
 
 Input: head = [1,2,3,4]
 Output: [2,1,4,3]
@@ -17,13 +14,11 @@ Example 3:
 Input: head = [1]
 Output: [1]
  
-
 Constraints:
 
-The number of nodes in the list is in the range [0, 100].
-0 <= Node.val <= 100
+    The number of nodes in the list is in the range [0, 100].
+    0 <= Node.val <= 100
  
-
 Follow up: Can you solve the problem without modifying the values in the list's nodes? (i.e., Only nodes themselves may be changed.)
 """
 
