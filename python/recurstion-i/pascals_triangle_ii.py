311ea8f63669f8f936b33e3e68d4e3c136b02144
Li, Jack <chl218@eng.ucsd.edu>
2021-05-06 17:22:41 -0700

    pascal

---
+++ python/recurstion-i/pascals_triangle_ii.py
@@ -0,0 +1,43 @@
+"""
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+
+Constraints:
+
+    0 <= rowIndex <= 33
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+
+"""
+
+import math
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+
+        # !n/(k!(n-k)!)
+        def nCk(n, k):
+            return math.factorial(n) // (math.factorial(k) * math.factorial(n-k))
+
+        row = []
+        for k in range(rowIndex+1):
+            row.append(nCk(rowIndex, k))
+
+        return row
