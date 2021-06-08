96c28557a42315f785ffa122cf24894b9df7ddfe
Li, Jack <chl218@eng.ucsd.edu>
2021-06-07 22:30:31 -0700

    min cost stair climb

---
+++ python/leetcoding-challenge/06-june/07_min_cost_climbing_stairs.py
@@ -0,0 +1,43 @@
+"""
+You are given an integer array cost where cost[i] is the cost of ith step on a
+staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+
+Example 1:
+Input: cost = [10,15,20]
+Output: 15
+Explanation: Cheapest is: start on cost[1], pay that cost, and go to the top.
+
+Example 2:
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: Cheapest is: start on cost[0], and only step on 1s, skipping
+             cost[3].
+
+
+Constraints:
+    2 <= cost.length <= 1000
+    0 <= cost[i] <= 999
+
+"""
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        n = len(cost)
+
+        dp = [0] * (n + 1)  # dp[i] is minimum cost to reach to i_th floor
+
+        for i in range(2, n + 1):
+            # Minimum cost if we jump 1 step from floor (i-1)_th to i_th floor
+            jumpOneStep = dp[i - 1] + cost[i - 1]
+            # Minimum cost if we jump 2 steps from floor (i-2)_th to i_th floor
+            jumpTwoStep = dp[i - 2] + cost[i - 2]
+            dp[i] = min(jumpOneStep, jumpTwoStep)
+
+        return dp[n]
