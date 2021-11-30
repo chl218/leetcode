c855fde364116cc6163ac71e024e2e692d7f46be
Li, Jack <chl218@eng.ucsd.edu>
2021-11-30 15:18:30 -0800

    dynamic stair climbing

---
+++ python/dynamic-programming/02_climbing_stairs.py
@@ -0,0 +1,27 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """ Dynamic Climbing Stairs
+
+        You are climbing a staircase. It takes n steps to reach the top. Each
+        time you can either climb 1 or 2 steps. In how many distinct ways can
+        you climb to the top?
+
+        Input: n = 3
+        Output: 3
+        Explanation: There are three ways to climb to the top.
+            1. 1 step + 1 step + 1 step
+            2. 1 step + 2 steps
+            3. 2 steps + 1 step
+
+        """
+        S = [1, 2]
+
+        if n <= 2:
+            return n
+
+        soln = None
+        for i in range(3, n+1):
+            soln = S[0] + S[1]
+            S[0], S[1] = S[1], soln
+
+        return soln
