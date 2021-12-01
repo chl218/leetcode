0d9c9939f2642457add90f8661049c27c836410e
Li, Jack <chl218@eng.ucsd.edu>
2021-11-30 23:19:10 -0800

    min cost climbing stairs

--- python/dynamic-programming/02a_climbing_stairs.py
+++ python/dynamic-programming/02a_climbing_stairs.py
@@ -6,6 +6,15 @@ class Solution:
         time you can either climb 1 or 2 steps. In how many distinct ways can
         you climb to the top?
 
+        Example 1:
+        Input: n = 2
+        Output: 2
+        Explanation: There are two ways to climb to the top.
+            1. 1 step + 1 step
+            2. 2 steps
+
+
+        Example 2:
         Input: n = 3
         Output: 3
         Explanation: There are three ways to climb to the top.
@@ -13,15 +22,17 @@ class Solution:
             2. 1 step + 2 steps
             3. 2 steps + 1 step
 
+        Constraints:
+            1 <= n <= 45
         """
-        S = [1, 2]
 
         if n <= 2:
             return n
 
+        s1, s2 = 1, 2
         soln = None
         for i in range(3, n+1):
-            soln = S[0] + S[1]
-            S[0], S[1] = S[1], soln
+            soln = s1 + s2
+            s1, s2 = s2, soln
 
         return soln
