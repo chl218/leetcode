b006e3623f07ec2800d2d644cb69d630e8472af2
Li, Jack <chl218@eng.ucsd.edu>
2022-02-28 19:55:19 -0800

    best opt

--- python/stuff/338-counting-bits.py
+++ python/stuff/338-counting-bits.py
@@ -50,5 +50,16 @@ class Solution:
 
             res.append(cnt)
 
-
         return res
+
+
+# class Solution {
+# public:
+#     vector<int> countBits(int n) {
+#         vector<int> res(n+1, 0);
+#         for (int i = 1; i <= n; ++i) {
+#             res[i] = res[i >> 1] + (i & 1);
+#         }
+#         return res;
+#     }
+# };
