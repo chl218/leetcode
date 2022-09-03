8fc3e76e1a7087688599502e6833e6687e082317
Li, Jack <chl218@eng.ucsd.edu>
2022-09-02 20:58:54 -0700

    math stuff

---
+++ python/stuff/1015_smallest_integer_divisble_by_k.py
@@ -0,0 +1,45 @@
+"""
+1015. Smallest Integer Divisible by K
+
+Given a positive integer k, you need to find the length of the smallest
+positive integer n such that n is divisible by k, and n only contains the
+digit 1.
+
+Return the length of n. If there is no such n, return -1.
+
+Note: n may not fit in a 64-bit signed integer.
+
+
+Example 1:
+Input: k = 1
+Output: 1
+Explanation: The smallest answer is n = 1, which has length 1.
+
+
+Example 2:
+Input: k = 2
+Output: -1
+Explanation: There is no such positive integer n divisible by 2.
+
+
+Example 3:
+Input: k = 3
+Output: 3
+Explanation: The smallest answer is n = 111, which has length 3.
+
+Constraints:
+    1 <= k <= 10^5
+
+"""
+
+class Solution:
+    def smallestRepunitDivByK(self, K):
+
+        if K % 2 == 0 or K % 5 == 0:
+            return -1
+
+        r = 0
+        for N in range(1, K + 1):
+            r = (r * 10 + 1) % K
+            if not r:
+                return N
