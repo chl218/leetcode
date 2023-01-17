3bcc3fe9e00ed7e6a05b1777e81ebabff72e33fb
Li, Jack <chl218@eng.ucsd.edu>
2023-01-16 23:17:19 -0800

    202 happy number

---
+++ python/study-plan/lc-75-level-2/01-happy-number.py
@@ -0,0 +1,55 @@
+"""
+202. Happy Number
+
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+    - Starting with any positive integer, replace the number by the sum of the
+      squares of its digits.
+    - Repeat the process until the number equals 1 (where it will stay), or it
+      loops endlessly in a cycle which does not include 1.
+    - Those numbers for which this process ends in 1 are happy.
+
+Return true if n is a happy number, and false if not.
+
+
+Example 1:
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+
+Example 2:
+Input: n = 2
+Output: false 
+
+
+Constraints:
+    1 <= n <= 2^31 - 1
+
+"""
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        num_set = set()
+
+        while n not in num_set:
+            num_set.add(n)
+
+            happy = 0
+            while n:
+                r = n % 10
+                happy += r*r
+                n = n // 10
+
+            if happy == 1:
+                return True
+            n = happy
+
+        return False
